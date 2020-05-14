733a8dac664894023935e38f56e116b80c36b62f
David Hutchison <dhutchis68516@gmail.com>
2020-05-14 19:55:40 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,13 +12,13 @@ class Dog
     @@all
   end
 
+  def print_all
+    @@all.map {|dog| puts dog.name}
+  end
+
   def self.clear_all
     @@all.clear
   end
 
-  def print_all
-    @@all.each do |dog|
-      puts dog.name
-    end
-  end
+
 end
